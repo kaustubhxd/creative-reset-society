81265823d2e9b61979f5d18bfa6705112a2a0252
Kaustubh <kaustubhpb@gmail.com>
2023-04-01 23:44:36 +0530

    style: text glitch effect

--- src/folds/home/HeroFold.tsx
+++ src/folds/home/HeroFold.tsx
@@ -17,20 +17,29 @@ const HeroFold = (props: Props) => {
                 backgroundColor: 'rgba(0,0,0,0.3)'
             }}
         >
-            <div className='mt-20'>
-                <div className='flex-center gap-5 text-6xl'>
+            <div className=''>
+
+            
+                
+                {/* <div className='flex-center gap-5 text-6xl'>
                     <div>creative</div>
                     <div className='mt-3 text-4xl'>•</div>
                     <div>reset</div>
                     <div className='mt-3 text-4xl'>•</div>
                     <div>society</div>
-                </div>
-                <div className='w-[50vw] text-center text-xl'>
-                    Our planet is in grave danger.
-                    We believe that by fostering creativity, empathy, and collaboration, we can bring about meaningful change 
-                    that benefits both humanity and the planet. Our goal is to provide a space where individuals from diverse backgrounds 
-                    can come together to share ideas, collaborate on innovative solutions, and create a more sustainable and equitable world.
-                    Launching soon.
+                </div> */}
+                
+                <div className='w-full flex-center gap-y-10 flex-col text-center '>
+                    <h2 className="hero glitch layers text-dm-serif" data-text="creative reset society">
+                        <span>creative reset society</span>
+                    </h2>
+                    <div className='w-[50%] text-xl'>
+                        Our planet is in grave danger.
+                        We believe that by fostering creativity, empathy, and collaboration, we can bring about meaningful change 
+                        that benefits both humanity and the planet. Our goal is to provide a space where individuals from diverse backgrounds 
+                        can come together to share ideas, collaborate on innovative solutions, and create a more sustainable and equitable world.
+                        Launching soon.
+                    </div>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'sans-serif'],
+        "dm-serif": ['"DM Serif Display"', 'serif']
+      }
+    },
   },
   plugins: [],
 }
